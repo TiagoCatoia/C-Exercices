47bdc38a9b2e8f97714e4f137cb30f7eb82dc72e
Tiago Catoia de Souza <133229612+TiagoCatoia@users.noreply.github.com>
2023-08-16 18:48:59 -0300

    Add files via upload

--- C Exercices/4 String/Nomes Ordem Crescente.c
+++ C Exercices/4 String/Nomes Ordem Crescente.c
@@ -1,4 +1,39 @@
+#include <stdio.h>
+#include <locale.h>
+#include <string.h>
+
 /*  EXERCICIO 3
 Faça um programa que leia 10 nomes, ordene-os de
 forma crescente e mostre-os na tela.
+*/
+
+main(){
+	setlocale(LC_ALL, "Portuguese");
+	char nomes[5][50];
+	int i, t, posicaoMenor, tamanhoMenor;
+	
+	printf("Digite 10 nomes para ordena-los em ordem crescente: \n");
+	
+	for(i=0; i<10; i++){
+		gets(nomes[i]);
+	}
+	
+	printf("\nNomes em ordem crescente: ");
+	for(t=0; t<10; t++){
+		posicaoMenor = -1;
+		tamanhoMenor = -1;
+		
+		for(i=0; i<10; i++){
+			if ((strlen(nomes[i]) < tamanhoMenor || tamanhoMenor == -1) && nomes[i][0] != '\0'){
+				posicaoMenor = i;
+				tamanhoMenor = strlen(nomes[i]);
+			}
+		}
+		printf("\n%s", nomes[posicaoMenor]);
+		nomes[posicaoMenor][0] = '\0';
+	}
+}
+/*
+Também podemos usar a função strcmp() para comparar as strings com base na ordem alfabética.
+Porém, precisaria criar um loop para encontrar a menor string em vez de comparar seus comprimentos.
 */
