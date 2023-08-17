665d7889ca7d906c787d7d9055cd228a1bb16ada
Tiago Catoia de Souza <133229612+TiagoCatoia@users.noreply.github.com>
2023-08-17 15:36:56 -0300

    Update Criptografia.c

--- C Exercices/4 String/Criptografia.c
+++ C Exercices/4 String/Criptografia.c
@@ -2,13 +2,13 @@
 #include <locale.h>
 #include <string.h>
 #include <ctype.h>
-/*  Lista de Exercícios – Algoritmos e Programação II
+/*  Lista de ExercÃ­cios â€“ Algoritmos e ProgramaÃ§Ã£o II
 
 EXERCICIO 11 da lista
-Faça um programa que receba um texto (string) como entrada e criptografa-o de 
-acordo com a tabela a seguir. O programa deverá imprimir o texto criptografado.
+FaÃ§a um programa que receba um texto (string) como entrada e criptografa-o de 
+acordo com a tabela a seguir. O programa deverÃ¡ imprimir o texto criptografado.
 Tabela de criptografia:
-	Representação:
+	RepresentaÃ§Ã£o:
 	-------------------------
 	|Vogais @				 |
 	-------------------------
@@ -17,27 +17,27 @@ Tabela de criptografia:
 	|Caracteres(@,#,$,%,etc) |
 	|especiais %			 |
 	-------------------------
-	|Dígitos 0-9 $			 |
+	|DÃ­gitos 0-9 $			 |
 	-------------------------
-	|Espaço *				 |
+	|EspaÃ§o *				 |
 	-------------------------
 Exemplo:
 Texto de entrada: Aprendendo a programar em C na disciplina de APR2
-Saída: 
+SaÃ­da: 
 @##@##@##@*@*##@##@#@#*@#*#*#@*#@##@##@#@*#@*@##$
 
 
- EXPLICAÇÃO Array de Ponteiros para Caracteres (Strings)
+ EXPLICAÃ‡ÃƒO Array de Ponteiros para Caracteres (Strings)
 char *vogais = "aeiou";:
-Nesse caso, você está criando um ponteiro para caracteres (char *) chamado vogais e inicializando-o com a string "aeiou".
-Isso significa que vogais apontará para o primeiro caractere da string (ou seja, 'a').
-A string "aeiou" é armazenada como uma sequência de caracteres na memória, terminada pelo caractere nulo ('\0').
-O ponteiro vogais aponta para o primeiro caractere dessa sequência.
+Nesse caso, vocÃª estÃ¡ criando um ponteiro para caracteres (char *) chamado vogais e inicializando-o com a string "aeiou".
+Isso significa que vogais apontarÃ¡ para o primeiro caractere da string (ou seja, 'a').
+A string "aeiou" Ã© armazenada como uma sequÃªncia de caracteres na memÃ³ria, terminada pelo caractere nulo ('\0').
+O ponteiro vogais aponta para o primeiro caractere dessa sequÃªncia.
 
 char *vogais[] = {"a", "e", "i", "o", "u"};:
-Neste caso, você está criando um array de ponteiros para caracteres (char *) chamado vogais e inicializando-o com um conjunto de strings.
-Cada elemento do array é um ponteiro que aponta para a primeira letra de cada uma das strings "a", "e", "i", "o" e "u".
-Assim, você tem um array que armazena vários ponteiros para strings diferentes.
+Neste caso, vocÃª estÃ¡ criando um array de ponteiros para caracteres (char *) chamado vogais e inicializando-o com um conjunto de strings.
+Cada elemento do array Ã© um ponteiro que aponta para a primeira letra de cada uma das strings "a", "e", "i", "o" e "u".
+Assim, vocÃª tem um array que armazena vÃ¡rios ponteiros para strings diferentes.
 */
 
 #define True 1
@@ -75,9 +75,8 @@ main(){
 				traduzido = True;
 			}	
 		}
-		/* comparação texto[i] == caracteresEspeciais[t] não funcionará corretamente para caracteres especiais,
-		porque o operador == compara caracteres individualmente e caracteres especiais podem ser representados por mais de um byte,
-		logo utilizo o strchr para verificar se texto[i] é algum caractere especial. (#include <string.h>)
+		/* comparaÃ§Ã£o texto[i] == caracteresEspeciais[t] nÃ£o funcionarÃ¡ corretamente para caracteres especiais,
+		porque o operador == compara caracteres individualmente e caracteres especiais podem ser representados por mais de um byte.
 		*/
 		for (t = 0; caracteresEspeciais[t] != '\0' && traduzido == False; t++) { 
 		    if (texto[i] == caracteresEspeciais[t]) {
